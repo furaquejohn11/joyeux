a2e8de6ed267c9392189a90d8ff917035d0b5d84
John Patrick Furaque <102823750+furaquejohn11@users.noreply.github.com>
2022-12-24 16:36:36 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+window.onload = () => {
+    document.getElementById("background").style.display = "none";
+    alert('Hello!');   
+    alert('I saw your shared post about flower');
+    alert('So I made a simple one for you :)');
+    alert('Actually I just copied the code from online :)');
+    alert('I know it is not that much');
+    alert('and you can find its source code online');
+    alert('but i hope you will like it!');
+    alert('....');
+    alert('Merry Christmas!');
+    document.getElementById("background").style.display = "block";
+}
