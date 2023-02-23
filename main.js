035ba453ed3ddc1866cdb196ce80bb0a4b80d640
John Patrick Furaque <102823750+furaquejohn11@users.noreply.github.com>
2023-02-23 21:40:58 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,13 +1,5 @@
 window.onload = () => {
     document.getElementById("background").style.display = "none";
-    alert('Hello!');   
-    alert('I saw your shared post about flower');
-    alert('So I made a simple one for you :)');
-    alert('Actually I just copied the code from online :)');
-    alert('I know it is not that much');
-    alert('and you can find its source code online');
-    alert('but i hope you will like it!');
-    alert('....');
-    alert('Merry Christmas!');
+    alert('Wala ako mailagay pero sige i-view mo nalang HAHAHHAHAH');
     document.getElementById("background").style.display = "block";
 }
